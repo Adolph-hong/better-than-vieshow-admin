fc5e4f83e0e99154716cf1dc46bb388269ce3ac6
iamlouxu <wayne199939@gmail.com>
2025-11-04 19:08:25 +0800

    feat: theaters router

--- Router.tsx
+++ Router.tsx
@@ -1,11 +1,16 @@
 // Router.tsx
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './src/components/pages/Home'
+import Theaters from './src/components/pages/Theaters'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/theaters',
+    element: <Theaters />,
   },
 ])
 
