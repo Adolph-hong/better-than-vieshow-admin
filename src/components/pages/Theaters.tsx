3c3d077e880b7a3d41dc617ac8b1c438bff37567
iamlouxu <wayne199939@gmail.com>
2025-11-04 20:17:52 +0800

    feat: add theaters header

--- src/components/pages/Theaters.tsx
+++ src/components/pages/Theaters.tsx
@@ -1,8 +1,14 @@
 const Theaters = () =>{
     return(
-        <>
-            
-        </>
+        <div className="flex">
+            <aside className="w-60 border-r border-gray-300 bg-gray-500 p-4">臨時側邊欄</aside>
+            <div className="flex-1">
+                <header className="flex items-center justify-between p-6 bg-[#E7E8EF]">
+                    <h1 className="font-bold text-[40px]">影廳</h1>
+                    <button type="button" className="inline-flex items-center justify-center w-24 h-10 bg-[#5365AC] text-white rounded-[10px] cursor-pointer">建立影廳</button>
+                </header>
+            </div> 
+        </div>
     )
 }
 
