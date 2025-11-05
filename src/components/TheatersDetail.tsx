daba274f8a0d28d4fd860698da452559e1db68ac
iamlouxu <wayne199939@gmail.com>
2025-11-05 15:03:19 +0800

    feat: install lucide

--- src/components/TheatersDetail.tsx
+++ src/components/TheatersDetail.tsx
@@ -1,6 +1,7 @@
 const TheatersDetail = () => {
   return (
-    <div>
+    <div className="flex">
+      <aside className="w-60 border-r border-gray-300 bg-gray-500 p-4">臨時側邊欄</aside>
       <h1>sssss</h1>
     </div>
   )
