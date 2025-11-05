45c9a9084f1590726ea0133feaae2cc91c493505
James <j606888@gmail.com>
2025-11-05 13:03:35 +0800

    Fix react router on Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from '../Router'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css'
+import Home from './components/pages/Home';
+import Theaters from './components/pages/Theaters';
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <nav className="flex gap-4">
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/theaters">Theaters</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/theaters" element={<Theaters />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
