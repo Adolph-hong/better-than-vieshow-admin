a4e86bb5b59556cea96de881ef6e806dec53473b
Adolph-hong <zxcv010806056@gmail.com>
2025-11-10 14:22:21 +0800

    update: finished sidebar top

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,16 @@
 const Sidebar = () => {
-  return <div className="min-h-screen w-60 bg-white py-6" />
+  return (
+    <div className="min-h-screen w-60 bg-white py-6">
+      {/* 上方logo與主題文字 */}
+      <section className="mb-11 flex justify-center gap-5">
+        <img src="/public/logo.svg" alt="logo" />
+        <div className="flex flex-col items-center">
+          <span className="body-small">Better Than</span>
+          <span className="body-large">威秀</span>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default Sidebar
