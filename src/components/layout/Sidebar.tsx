c7d1b719df60983977860e75d1a1f5e90c252eef
Adolph-hong <zxcv010806056@gmail.com>
2025-11-11 17:14:03 +0800

    style: updated sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,11 +5,11 @@ const Sidebar = () => {
   return (
     <aside className="flex min-h-screen w-60 flex-col bg-white py-6">
       {/* 上方logo與主題文字 */}
-      <section className="mb-11 flex justify-center gap-5">
+      <section className="mb-11 ml-4 flex gap-5 text-[#333333]">
         <img src="/public/logo.svg" alt="logo" />
         <div className="flex flex-col items-center">
-          <span className="body-small">Better Than</span>
-          <span className="body-large">威秀</span>
+          <span className="body-small font-bold">Better Than</span>
+          <span className="body-large font-semibold">威秀</span>
         </div>
       </section>
       {/* 中間選單 */}
@@ -18,22 +18,22 @@ const Sidebar = () => {
           <NavLink
             key={id}
             to={to}
-            className="flex items-center gap-4 py-5 hover:bg-[#F5F5F5] active:bg-[#6877D9]/6"
+            className="body-small flex items-center gap-4 py-5 leading-none text-gray-500 hover:bg-[#F5F5F5] active:bg-[#6877D9]/6"
           >
-            <Icon className="ml-9 text-gray-500" active:text-primary-500 />
+            <Icon className="ml-9" active:text-primary-500 />
             <span className="active:text-primary-500">{title}</span>
           </NavLink>
         ))}
       </section>
       {/* 下方會員 */}
-      <section className="mt-auto h-12 px-3">
+      <section className="mt-auto px-3">
         <div className="flex gap-3">
           <img src="/public/admin-photo.svg" alt="admin-photo" />
-          <div className="flex min-w-0 flex-1 flex-col gap-1">
-            <span className="font-family-inter leading-nore text-lg font-semibold text-[#000000]">
+          <div className="mt-auto flex h-[45px] min-w-0 flex-1 flex-col justify-end gap-1">
+            <span className="font-inter text-lg leading-none font-semibold text-[#000000]">
               Zhen Yu
             </span>
-            <span className="leading-nore font-family-inter truncate font-normal text-[#BAB9BE]">
+            <span className="font-inter truncate text-sm font-normal text-[#BAB9BE]">
               zhen-yu@gmail.com
             </span>
           </div>
