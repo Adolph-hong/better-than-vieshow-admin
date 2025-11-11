4433683abe65917a218b129030a34d6f2d0ed404
Adolph-hong <zxcv010806056@gmail.com>
2025-11-12 01:04:57 +0800

    feat: add movie components

---
+++ src/movie/Layout.tsx
@@ -0,0 +1,5 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex min-h-screen flex-1 flex-col bg-[#E7E8EF]">{children}</div>
+}
+
+export default Layout
