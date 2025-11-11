ea3d407d6124b177edcf0d361cc5244e8a179f1e
iamlouxu <wayne199939@gmail.com>
2025-11-11 13:17:49 +0800

    update: remove background color and add h1 word

--- src/components/SidebarToolbox.tsx
+++ src/components/SidebarToolbox.tsx
@@ -12,10 +12,10 @@ const SidebarToolbox = () => {
 
   return (
     <div className="relative w-fit">
-      <div className="pointer-events-none absolute -inset-3 rounded-2xl bg-indigo-50" />
       <aside
         className="relative w-[270px] h-[499px] bg-white border border-white rounded-sm p-3 space-y-4"
       >
+        <h1 className="header-3">座位表</h1>
         <div className="relative h-10 w-full rounded-sm bg-[#F7F7F7] p-1">
           <div
             className={`absolute inset-y-1 left-1 rounded-sm bg-white shadow transition-all duration-200 ${
