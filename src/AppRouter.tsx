9d7d6135488ebfe7d98d0214c91d4ac9255e21c9
Adolph-hong <zxcv010806056@gmail.com>
2025-11-12 01:12:29 +0800

    remove: delete theater page and router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '@/components/pages/Home'
 import Movie from '@/components/pages/Movie'
-import Theaters from '@/components/pages/Theaters'
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/theaters" element={<Theaters />} />
       <Route path="/movie" element={<Movie />} />
     </Routes>
   )
