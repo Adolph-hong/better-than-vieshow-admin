6c16e26f9be22b8466f214f9d264e1e69016af3e
iamlouxu <wayne199939@gmail.com>
2025-12-26 15:59:41 +0800

    refactor: enhance accessibility and semantic structure

--- pages/theaters/Theaters.tsx
+++ pages/theaters/Theaters.tsx
@@ -49,16 +49,20 @@ const Theaters = () => {
 
   return (
     <AdminContainer>
-      <Header title="影廳" buttonText="建立影廳" onClick={() => navigate("/theaters/new")} />
-      {theaters.length === 0 ? (
-        <EmptyContent title="一間影廳都還沒有" description="點擊「建立影廳」來新增第一間吧" />
-      ) : (
-        <div className="grid grid-cols-1 gap-6 px-6 md:grid-cols-2 lg:grid-cols-3">
-          {theaters.map((theater) => (
-            <TheaterCard key={theater.id} theater={theater} onDelete={handleDelete} />
-          ))}
-        </div>
-      )}
+      <main className="flex flex-1 flex-col">
+        <Header title="影廳" buttonText="建立影廳" onClick={() => navigate("/theaters/new")} />
+        {theaters.length === 0 ? (
+          <EmptyContent title="一間影廳都還沒有" description="點擊「建立影廳」來新增第一間吧" />
+        ) : (
+          <ul className="grid grid-cols-1 gap-6 px-6 md:grid-cols-2 lg:grid-cols-3">
+            {theaters.map((theater) => (
+              <li key={theater.id}>
+                <TheaterCard theater={theater} onDelete={handleDelete} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </AdminContainer>
   )
 }
